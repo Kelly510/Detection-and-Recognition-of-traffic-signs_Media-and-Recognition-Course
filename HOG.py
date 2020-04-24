9693299c8a9ed81b05c7032b6cda3dd610452269
Kelly510 <49122590+Kelly510@users.noreply.github.com>
2020-04-24 08:19:19 +0800

    Update HOG.py

--- HOG.py
+++ HOG.py
@@ -2,29 +2,32 @@ from PIL import Image
 import numpy as np
 import cv2
 
-def RGBA_to_stdGray(image, width=128, height=128):
-    # 传入一张RGBA四通道图象的tensor，转换为相同大小的灰度图
-    # 输入：高*宽*通道数(4)
-    # 输出：标准高*标准宽*1
-    # Attention：标准高和标准宽的大小有待调整
-    image = image.resize((width,height),Image.ANTIALIAS)
-    (b, g, r, a) = cv2.split(image)
+def RGB_to_stdGray(image):
+    # 传入一张RGB图象的np.array，转换为相同大小的灰度图
+    # 考虑到伽马矫正的效果很差，暂时没有做这一步骤
+    # 输入：高*宽*通道数(3)
+    # 输出：高*宽
+    (b, g, r) = cv2.split(image)
     gray = 0.114 * b + 0.587 * g + 0.299 * r
-    gray = gray.astype(np.uint8)
+    gray = gray.astype(np.float32)
+    # 下面两行可以测试灰度函数的有效性
+    # outputImg=Image.fromarray(gray*255.0)
+    # outputImg.show()
     return gray
 
 def getHOGfeat(image, stride = 8, orientations = 8, pixels_per_cell = (8, 8), cells_per_block = (2, 2)):
     # 传入一张RGBA四通道图象的tensor，转换为提取了HOG特征的矩阵
     # 输入：高*宽*通道数(4)
-    # 输出：
-    image = RGBA_to_stdGray(image) # 转换为标准大小灰度图
+    # 输出：n_blocksx, n_blocksy, bx * by * orientations
+    # 输出默认情况下为14*14*32
+    image = RGB_to_stdGray(image) # 转换为标准大小灰度图
     cx, cy = pixels_per_cell
     bx, by = cells_per_block
     sx, sy = image.shape
     n_cellsx = sx // cx # x方向上cell的个数
     n_cellsy = sy // cy # y方向上cell的个数
-    n_blocksx = n_cellsx - bx + 1 # x方向上block的个数
-    n_blocksy = n_cellsy - by + 1 # y方向上block的个数
+    n_blocksx = (n_cellsx - bx) * cx // stride # x方向上block的个数
+    n_blocksy = (n_cellsy - by) * cy //stride # y方向上block的个数
     gx = np.zeros((sx, sy), dtype = np.float32) # x方向上的梯度矩阵
     gy = np.zeros((sx, sy), dtype = np.float32) # y方向上的梯度矩阵
     eps = 1e-5
@@ -46,17 +49,16 @@ def getHOGfeat(image, stride = 8, orientations = 8, pixels_per_cell = (8, 8), ce
     normalised_blocks = np.zeros((n_blocksx, n_blocksy, bx * by * orientations))
     for y in range(n_blocksy):
         for x in range(n_blocksx):
-            block = grad[y*stride : y*stride+16, x*stride : x*stride+16]
+            block = grad[y*stride : y*stride+cy*by, x*stride : x*stride+cx*bx]
             hist_block = np.zeros(32, dtype = np.float32)
             for k in range(by):
                 for m in range(bx):
-                    cell = block[k*8 : (k+1)*8, m*8 :(m+1)*8]
-                    hist_cell = np.zeros(8,dtype = np.float32)
+                    cell = block[k*cy : (k+1)*cy, m*cx :(m+1)*cx]
+                    hist_cell = np.zeros(orientations,dtype = np.float32)
                     for i in range(cy):
                         for j in range(cx):
-                            n = int(cell[i,j,0] / 45)
+                            n = int(cell[i,j,0] * orientations // 360)
                             hist_cell[n] += cell[i,j,1]
                     hist_block[(k*bx+m)*orientations:(k*bx+m+1)*orientations] = hist_cell[:]
             normalised_blocks[y, x, :] = hist_block / np.sqrt(hist_block.sum() ** 2 + eps)
-    return normalised_blocks.ravel()
-            
+    return normalised_blocks
